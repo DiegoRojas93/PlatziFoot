8813d7f315b1f54935817befcbc953aa0608ff13
Diego Rojas <diegorojas431@gmail.com>
2020-05-21 17:23:08 -0500

    Personalizació y configuración

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      },
+      margin: {
+        '96': '24rem',
+        '128': '32rem',
+      },
+    }
   },
   variants: {},
   plugins: [],
